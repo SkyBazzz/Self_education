ca3f8ac96e3e582d7dd120fba3811b8591b91ba5
Oleksandr Balkashyn <oleksandr_balkashyn@epam.com>
2017-03-28 14:37:28 +0300

    //TODO MorseCodeDecoder

---
+++ src/main/java/codewars/MorseCodeDecoder.java
@@ -0,0 +1,8 @@
+package codewars;
+
+public class MorseCodeDecoder {
+    public static String decode(String morseCode) {
+        // your brilliant code here, remember that you can access the preloaded Morse code table through MorseCode.get(code)
+        return "";
+    }
+}
