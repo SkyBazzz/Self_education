fdf7b3fd5f36e3333aedc158d798a37a41e128fd
Oleksandr Balkashyn <skyaleksandr@gmail.com>
2017-04-17 20:02:11 +0300

    sonar fixes

--- src/main/java/patterns/builder/BuilderDemo.java
+++ src/main/java/patterns/builder/BuilderDemo.java
@@ -1,5 +1,6 @@
 package patterns.builder;
 
+import patterns.builder.builder.FordBuilder;
 import patterns.builder.builder.SubaruBuilder;
 import patterns.builder.car.Car;
 import patterns.builder.director.Director;
@@ -8,8 +9,11 @@ public class BuilderDemo {
     public static void main(String[] args) {
         Director director = new Director();
         director.setBuilder(new SubaruBuilder());
-        Car car = director.buildCar();
-        System.out.println(car);
+        Car car1 = director.buildCar();
+        System.out.println(car1);
 
+        director.setBuilder(new FordBuilder());
+        Car car2 = director.buildCar();
+        System.out.println(car2);
     }
 }
