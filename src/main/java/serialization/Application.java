04fc7bed9280228b918a444dfb507f55d452c525
Oleksandr Balkashyn <oleksandr_balkashyn@epam.com>
2018-01-02 16:51:40 +0200

    basic principle applied

--- src/main/java/serialization/Application.java
+++ src/main/java/serialization/Application.java
@@ -1,28 +1,32 @@
 package serialization;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class Application {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         DataObject dataObject = new DataObject();
-        dataObject.setMyData("Very loooooooooooooooooooooooooong daa");
+        dataObject.setMyData("Very loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong data");
         File file = new File("store.bin");
 
         FileOutputStream fo = new FileOutputStream(file);
-        ObjectOutputStream objOutputStream = new ObjectOutputStream(fo);
-        objOutputStream.writeObject(dataObject);
-        objOutputStream.flush();
-        objOutputStream.close();
+
+        try (ObjectOutputStream objOutputStream = new ObjectOutputStream(fo)) {
+            objOutputStream.writeObject(dataObject);
+            objOutputStream.flush();
+        }
 
         FileInputStream fi = new FileInputStream(file);
-        ObjectInputStream is = new ObjectInputStream(fi);
-        DataObject objNew = (DataObject) is.readObject();
-        is.close();
+        DataObject objNew;
+        try (ObjectInputStream is = new ObjectInputStream(fi)) {
+            objNew = (DataObject) is.readObject();
+        }
 
         System.out.println(objNew.toString());
-        System.out.println(objNew.toString());
-
-
-
+        System.out.println(objNew.getMyData());
     }
 }
