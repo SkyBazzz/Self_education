f72f5b506d49faac30518832417887978d90b34a
Oleksandr_Balkashyn <skyaleksandr@gmail.com>
2018-07-03 22:32:38 +0300

    2 tasks

---
+++ src/main/java/codewars/SumDigPower.java
@@ -0,0 +1,31 @@
+package codewars;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class SumDigPower {
+
+    public static List<Long> sumDigPow(long a, long b) {
+        List<Long> list = new ArrayList<>();
+
+        for (long i = a; i < b; i++) {
+            if (isEqualToPowersSum(i)) {
+                list.add(i);
+            }
+        }
+
+        return list;
+    }
+
+    private static boolean isEqualToPowersSum(long n) {
+        long sum = 0;
+        String numString = String.valueOf(n);
+
+        for (int i = 0; i < numString.length(); i++) {
+            int digit = Integer.parseInt(numString.charAt(i) + "");
+            sum += Math.pow(digit, i + 1);
+        }
+
+        return n == sum;
+    }
+}
